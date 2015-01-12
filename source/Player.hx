a0078915bdec85d482129cee7e2f5b9efe4639a5
Marcel Champagne <voiceofmarcel@gmail.com>
2015-01-11 18:08:18 -0800

    Player movement, sprite, and partial rotation

--- source/Player.hx
+++ source/Player.hx
@@ -12,44 +12,57 @@ class Player extends FlxSprite
 {
 	
 	var speed:Float;
+	var playerFriction:Float;
 	
 	public function new(x:Float=0, y:Float) 
 	{
 		super(x, y);
-		makeGraphic(16, 16);
-		speed = 5;
+		loadGraphic(AssetPaths.doctor__png);
+		speed = .5;
+		playerFriction = .025;
 		
 	}
 	
 	public function movement() 
 	{
 		if (FlxG.keys.pressed.A) {
-			velocity.x = -speed;
+			acceleration.x = -speed;
 		}
 		else if (FlxG.keys.pressed.D) {
-			velocity.x = speed;
+			acceleration.x = speed;
 		}
 		else {
-			velocity.x = 0;
+			acceleration.x = 0;
+			velocity.x /= 1.1;
 		}
 		
 		if (FlxG.keys.pressed.W) {
-			velocity.y = -speed;
+			acceleration.y = -speed;
 		}
 		else if (FlxG.keys.pressed.S) {
-			velocity.y = speed;
+			acceleration.y = speed;
 		}
 		else {
-			velocity.y = 0;
+			acceleration.y = 0;
+			velocity.y /= 1.1;
 		}
 		
 		if ( (FlxG.keys.pressed.A || FlxG.keys.pressed.D) && (FlxG.keys.pressed.W ||FlxG.keys.pressed.S) ) {
-			velocity.x /= Math.sqrt(speed * speed * 2);
-			velocity.y /= Math.sqrt(speed * speed * 2);
-			velocity.x *= speed;
-			velocity.y *= speed;
+			acceleration.x /= Math.sqrt(speed * speed * 2);
+			acceleration.y /= Math.sqrt(speed * speed * 2);
+			acceleration.x *= speed;
+			acceleration.y *= speed;
 		}
 		
+		
+		angle = velocity.x * 4.5;
+		if (velocity.y > 0)
+			angle *= 2;
+			
+		
+			
+		velocity.x += acceleration.x - playerFriction*velocity.x;
+		velocity.y += acceleration.y - playerFriction*velocity.y;
 		x += velocity.x;
 		y += velocity.y;
 		
