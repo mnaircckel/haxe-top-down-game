1bef0ca9081b3297183eb4cdbe5d09a21f3fb1e0
Nicolas Knoebber <knoebber@gmail.com>
2015-01-12 16:13:26 -0800

    scaled enemy

--- source/Enemy.hx
+++ source/Enemy.hx
@@ -16,7 +16,7 @@ class Enemy extends FlxSprite
 	{
 		super(x, y);
 		speed = 5;
-		loadGraphic(AssetPaths.enemy__png, true, 16, 16);
+		loadGraphic(AssetPaths.enemy__png, true, 64, 64);
 		animation.add("run", [0, 1, 2, 3,5,6,7,8,9],8, true);
 		velocity.x = 0;
 		velocity.y = 0;
