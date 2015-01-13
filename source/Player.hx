8f65b6812c1467543757f797118fd89ee3b017cb
Marcel Champagne <voiceofmarcel@gmail.com>
2015-01-12 16:02:20 -0800

    added full animations for doctor and updated sprite

--- source/Player.hx
+++ source/Player.hx
@@ -16,20 +16,67 @@ class Player extends FlxSprite
 	var speed:Float;
 	var playerFriction:Float;
 	var aimAngle:Float;
+	var isWalking:Bool;
+	var isAttacking:Bool;
+	var  attackTimer:Int;
 	public function new(x:Float, y:Float) 
 	{
 		super(x, y);
 		loadGraphic(AssetPaths.doctor__png,true,64,64);
 		speed = .20;
 		playerFriction = .030;
-		animation.add("right", [0,1,2],8,true);
-		animation.play("right");
+		isWalking = false;
+		isAttacking = false;
+		attackTimer = 0;
+		animation.add("idle", [7, 8], 2, true);
+		animation.add("walking", [3, 4, 5, 6], 8, true);
+		animation.add("attack", [0, 1], 4, true);
+		animation.add("dead", [2], 1, true);
+		animation.play("idle");
 		
 	
 	}
 	
+	public function animations() {
+		
+		
+		if (isAttacking) {
+			++attackTimer;
+			if (attackTimer > 30) {
+				attackTimer = 0;
+				isAttacking = false;
+				animation.play("idle");
+			}
+		}
+		
+		
+		if (FlxG.mouse.justPressed) {
+			animation.play("attack",true);
+			isWalking = false;
+			isAttacking = true;
+		}
+		else if ( (Math.abs(velocity.x) + Math.abs(velocity.y)) > .5 ) {
+			if (!isWalking && !isAttacking) {
+				animation.play("walking");
+			}
+			isWalking = true;
+		}
+		else {
+			if (isWalking) {
+				animation.play("idle");
+			}
+			isWalking = false;
+		}
+	
+		
+		
+		
+	}
+	
+	
 	public function movement() 
-	{
+	{	
+		
 		if (FlxG.keys.pressed.A) {
 			acceleration.x = -speed;
 		}
@@ -92,6 +139,7 @@ class Player extends FlxSprite
 	{
 
 		movement();
+		animations();
 		super.update();
 	}
 	
