bdc0440152bf6f31e927f2bb19b8293bcc924235
Nicolas Knoebber <knoebber@gmail.com>
2015-01-12 19:08:47 -0800

    added rotation and animations to enemy

--- source/Enemy.hx
+++ source/Enemy.hx
@@ -3,15 +3,18 @@ package ;
 import flixel.FlxSprite;
 import flixel.FlxG;
 import flixel.FlxObject;
+import Math;
 
 /**
  * Class for handling Enemies. 
  */
 class Enemy extends FlxSprite
 {
-	
-	var speed:Float;
 	var idle = true;
+	var playingRun = true;
+	var playingAttack=false;
+	var speed:Float;
+	
 	var attacking = false;
 	var target:FlxSprite;
 	var walkCount=0;
@@ -20,6 +23,8 @@ class Enemy extends FlxSprite
 	//the x and y distance to the target. 
 	var deltaX:Float;
 	var deltaY:Float;
+	var theta:Float;
+
 	public function new(x:Float, y:Float,targ:FlxSprite) 
 	{
 		super(x, y);
@@ -28,7 +33,7 @@ class Enemy extends FlxSprite
 		loadGraphic(AssetPaths.enemy__png, true, 64, 64);
 		animation.add("run", [3, 4, 5, 6],8, true);
 		animation.add("death", [2],1, true);
-		animation.add("attack", [0,1],5, false);
+		animation.add("attack", [0,1,3,5],14, true);
 		velocity.x = 0;
 		velocity.y = 0;
 		animation.play("run");
@@ -47,12 +52,17 @@ class Enemy extends FlxSprite
 	public function movement():Void
 	{
 
-		if(distance< 60)
+		if(distance<250)
 			idle = false;
+		else{
+			idle = true;
+			
+		}
 
 		//move in a shitty square. feel free to change. 
 		if(idle)
 		{
+
 			walkCount++;
 			if(walkCount<50){
 				velocity.x = 1;
@@ -77,11 +87,30 @@ class Enemy extends FlxSprite
 			else if(walkCount>=200)
 				walkCount = 0;
 		}
-		else //if it is close then attack. Once it is targetted it will never stop.
+		else //if it is close then attack. 
 		{		
 			//make into unit vector so we can move towards target
+			if(distance<75 && !playingAttack){
+				animation.play("attack");
+				playingAttack=true;
+				playingRun=false;
+			}
+			if(distance>75 && !playingRun){
+					animation.play("run");
+					playingRun=true;
+					playingAttack=false;
+				}
+
 			deltaX/=distance; 
 			deltaY/=distance;
+			
+			//calculate rotation
+			theta = (Math.acos(deltaY) *(180/Math.PI));
+			if(deltaX>0)
+				theta*=-1;
+
+			angle=theta;
+			//trace(deltaX);
 			velocity.x= deltaX;
 			velocity.y = deltaY;
 		}
